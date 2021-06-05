a933805501ff9e0c820bf08106433e395fff2ff2
unknown <n2nychannel@gmail.com>
2021-06-05 19:04:25 +0530

    Intial commit

---
+++ Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.d
@@ -0,0 +1,71 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ F:/Project/Projects_STM32/led-basic-y/Inc/stm32f0xx_hal_conf.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cm0.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cmInstr.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/cmsis_gcc.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cmFunc.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+F:/Project/Projects_STM32/led-basic-y/Inc/stm32f0xx_hal_conf.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cm0.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cmInstr.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include/core_cmFunc.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
