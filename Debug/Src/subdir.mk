87763bdcf0baa6699b2517c0213c660a3ef8a46b
unknown <n2nychannel@gmail.com>
2021-06-13 23:06:13 +0530

    Update with Switch

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -30,7 +30,7 @@ Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F030x8 -I"F:/Project/Projects_STM32/led-basic-y/Inc" -I"F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc" -I"F:/Project/Projects_STM32/led-basic-y/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"F:/Project/Projects_STM32/led-basic-y/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F030x8 -I"F:/Project/Projects_STM32/video-tutorials/Inc" -I"F:/Project/Projects_STM32/video-tutorials/Drivers/STM32F0xx_HAL_Driver/Inc" -I"F:/Project/Projects_STM32/video-tutorials/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"F:/Project/Projects_STM32/video-tutorials/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"F:/Project/Projects_STM32/video-tutorials/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Src/main.c
+++ Src/main.c
@@ -68,7 +68,7 @@ int main(void)
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
-  
+	GPIO_PinState pinread;
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -97,9 +97,15 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
-	  HAL_Delay(5000);
-
+	  pinread = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+	 	  		if(pinread == GPIO_PIN_SET)
+	 	  	  	  {
+	 	  			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET);
+	 	  	  	  }
+	 	  		else
+	 	  		{
+	 	  			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET);
+	 	  		}
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -148,10 +154,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
